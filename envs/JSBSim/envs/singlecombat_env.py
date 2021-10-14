Bugfix: wrong unit conversion for missile rendering

envs/JSBSim/envs/singlecombat_env.py
@@ -137,12 +137,14 @@ class SingleCombatEnv(BaseEnv):
                 self.sims[agent_id].close()
 
     def render(self, mode="human"):
-        # TODO: real time rendering
+        # TODO: real time rendering [Use FlightGear]
         render_list = []
         for agent_id in range(self.num_fighters):
-            render_list.append(np.array(self.sims[agent_id].get_property_values(self.task.render_var))) # flight
+            # flight
+            render_list.append(np.array(self.sims[agent_id].get_property_values(self.task.render_var)))
+            # missile
             if 'missile' in self.taskname:
-                render_list.extend(self.task.render(agent_id)) # missile
+                render_list.extend(self.task.get_missile_trajectory(self, agent_id))
         return np.hstack(render_list)
 
     def seed(self, seed):

envs/JSBSim/tasks/singlecombat_with_missle_task.py
@@ -50,25 +50,8 @@ class SingleCombatWithMissileTask(SingleCombatTask):
         """Reset fighter blood & missile status
         """
         self.bloods = [100 for _ in range(self.num_agents)]
-        self.missile_lists = [Missile3D() for _ in range(self.num_agents)]  # By default, both figher has 2 missiles.
+        self.missile_lists = [Missile3D() for _ in range(self.num_agents)]  # By default, both figher has 1 missile.
         return super().reset(env)
-    
-    def render(self, agent_id):
-        """
-        render missile trajectory, if no missile, use default value instead
-        """
-        missile_render = []
-        for i in range(self.missile_lists[agent_id].num_missile):
-            missile_state = np.zeros((6,))
-            if self.missile_lists[agent_id].missile_info[i]['flying']:
-                missile_state[:3] = np.array(self.missile_lists[agent_id].missile_info[i]['current_state'][:3])
-                missile_state[0], missile_state[1] = dis2lonlat(*missile_state[0:2], 120, 60) # meter to degree
-                missile_state[2] = missile_state[2] / 0.304 # meter to feet
-                #print("missile state:", missile_state[0:3])
-            else:
-                missile_state[:3] = np.array([120,0,20000])
-            missile_render.append(missile_state)
-        return missile_render
 
     def step(self, env, action):
         for agent_id in range(self.num_agents):
@@ -92,3 +75,22 @@ class SingleCombatWithMissileTask(SingleCombatTask):
             elif done and self.missile_lists[agent_id].check_no_missile_alive():
                 break
         return done, info
+
+    def get_missile_trajectory(self, env, agent_id):
+        """
+        render missile trajectory, if no missile, use default value instead
+        """
+        missile_render = []
+        for i in range(self.missile_lists[agent_id].num_missile):
+            missile_state = np.zeros((6,))
+            if self.missile_lists[agent_id].missile_info[i]['flying']:
+                missile_state[:3] = np.array(self.missile_lists[agent_id].missile_info[i]['current_state'][:3])
+            elif not self.missile_lists[agent_id].missile_info[i]['launched']:
+                missile_state[:3] = env.sims[agent_id].get_position()
+            else:
+                missile_state[:3] = np.array([120, 0, 20000])
+            # unit conversion (m, m, m) => (degree, degree, ft)
+            missile_state[0], missile_state[1] = dis2lonlat(*missile_state[0:2][::-1], 120, 60)
+            missile_state[2] = missile_state[2] / 0.304
+            missile_render.append(missile_state)
+        return missile_render

envs/JSBSim/utils/utils.py
@@ -27,13 +27,11 @@ def get_root_dir():
 
 
 def lonlat2dis(lon, lat, init_lon, init_lat):
-    """Convert longitude&latitude into xy distance
+    """Convert longitude-latitude into east-north distance
 
     Args:
-        lon (float): lontitude of current point
-        lat (float): latitude of current point
-        init_lon (float): lontitude of original point
-        init_lat (float): latitude of original point
+        lon/lat (float): lontitude/latitude of current point
+        init_lon/init_lat (float): lontitude/latitude of original point
 
     Returns:
         (np.array): (east, north), unit: m
@@ -43,13 +41,18 @@ def lonlat2dis(lon, lat, init_lon, init_lat):
     return np.array([east, north]) * 1000
 
 
-def dis2lonlat(x, y, init_lon, init_lat):
+def dis2lonlat(east, north, init_lon, init_lat):
+    """Convert east-north distance into longitude-latitude
+
+    Args:
+        east/north (float): coordinates in east-north distance system (unit: m)
+        init_lon/init_lat (float): lontitude/latitude of original point
+
+    Returns:
+        (np.array): (lon, lat), unit: degree
     """
-    """
-    x = x / 1000
-    y = y / 1000
-    lon = np.rad2deg((x / 6371 / 1.734 / np.cos(np.deg2rad(init_lat))) + np.deg2rad(init_lon))
-    lat = np.rad2deg(y * 11.11949266 / 11.1319 / 6371 + np.deg2rad(init_lat))
+    lon = np.rad2deg((east / 1000 / 6371 / 1.734 / np.cos(np.deg2rad(init_lat))) + np.deg2rad(init_lon))
+    lat = np.rad2deg(north / 1000 * 11.11949266 / 11.1319 / 6371 + np.deg2rad(init_lat))
     return np.array([lon, lat])
 
 