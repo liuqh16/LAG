829610663ca3972a3563895ac3bbe61be9595894
Jackory <1579823519@qq.com>
2021-11-26 17:57:10 +0800

    the latest train jsbsim

--- scripts/render_jsbsim.sh
+++ scripts/render_jsbsim.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-env="SingleCombat"     # JSBSim or SingleControl
-scenario="1v1/Missile/Selfplay"     # singlecombat_task or heading_task or heading_altitude_task.py
-num_agents=2
+env="SingleCombat"
+scenario="1v1/Missile/HierarchyVsBaseline"    
+num_agents=1
 algo="ppo"
-exp="render"
+exp="opposite_15km_attack12000"
 seed=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, seed is ${seed}"
 python render/render_jsbsim.py \
     --env-name ${env} --algorithm-name ${algo} --scenario-name ${scenario} --experiment-name ${exp} --num-agents ${num_agents} \
     --n-rollout-threads 1 \
-    --model-dir "D:\codelib\CloseAirCombat\scripts\results\SingleCombat\1v1\Missile\Selfplay\ppo\test\run1\models\97632000"\
+    --model-dir "/home/lqh/jyh/CloseAirCombat/scripts/results/SingleCombat/1v1/Missile/HierarchyVsBaseline/ppo/opposite_15km_attack12000/wandb/latest-run/files/95126400"\
     --seed ${seed}
--- scripts/train_jsbsim.sh
+++ scripts/train_jsbsim.sh
@@ -3,13 +3,13 @@ env="SingleCombat"
 scenario="1v1/Missile/HierarchyVsBaseline"
 num_agents=1
 algo="ppo"
-exp="pursue"
-seed=1
+exp="opposite_15km_attack8000"
+seed=2
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, seed is ${seed}"
-CUDA_VISIBLE_DEVICES=1 python train/train_jsbsim.py \
+CUDA_VISIBLE_DEVICES=2 python train/train_jsbsim.py \
     --env-name ${env} --algorithm-name ${algo} --scenario-name ${scenario} --experiment-name ${exp} --num-agents ${num_agents} \
-    --seed 1 --n-training-threads 1 --n-rollout-threads 32 --cuda\
+    --seed ${seed} --n-training-threads 1 --n-rollout-threads 32 --cuda\
     --num-mini-batch 5 --buffer-size 2700 --episode-length 900 --num-env-steps 1e8 \
     --lr 3e-4 --gamma 0.99 --ppo-epoch 4 --clip-params 0.2 --max-grad-norm 2 --entropy-coef 1e-3 \
     --hidden-size "128 128" --act-hidden-size "128 128" \
