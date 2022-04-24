cf62a27ced549f9e04ba722fdc261358c67bffad
Jackory <1579823519@qq.com>
2022-04-24 21:44:36 +0800

    update render.sh

--- scripts/render_selfplay.sh
+++ scripts/render_selfplay.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 env="SingleCombat"
-scenario="1v1/ShootMissile/HierarchySelfplay_nolimit"   
+scenario="1v1/ShootMissile/Selfplay"   
 algo="ppo"
-exp="fsp"
+exp="no_penalty_shoot"
 seed=2021
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, seed is ${seed}"
 python render/render_jsbsim.py \
     --env-name ${env} --algorithm-name ${algo} --scenario-name ${scenario} --experiment-name ${exp} \
     --act-hidden-size "128 128" \
-    --model-dir "/home/lqh/jyh/CloseAirCombat/scripts/results/SingleCombat/1v1/ShootMissile/HierarchySelfplay_nolimit/ppo/fsp/wandb/latest-run/files"\
+    --model-dir "/home/lqh/jyh/CloseAirCombat/scripts/results/SingleCombat/1v1/ShootMissile/Selfplay/ppo/no_penalty_shoot/wandb/latest-run/files"\
     --seed ${seed} \
-    --use-selfplay --use-eval --render-opponent-index "3124" --render-index "latest"
+    --use-prior \
+    --use-selfplay \
+    --use-eval --render-opponent-index "100" --render-index "latest"
